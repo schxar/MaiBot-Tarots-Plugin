5f8b2497dffdf0cd8f55a593e33c0b1f2e8171cd
A0000Xz <629995608@qq.com>
2025-07-13 18:19:18 +0800

    适配0.9.0

--- tarots_plugin/plugin.py
+++ tarots_plugin/plugin.py
@@ -1,4 +1,5 @@
-from src.plugin_system.base.base_plugin import BasePlugin, register_plugin
+from src.plugin_system.base.base_plugin import BasePlugin
+from src.plugin_system.apis.plugin_register_api import register_plugin
 from src.plugin_system.base.base_action import BaseAction, ActionActivationType, ChatMode
 from src.plugin_system.base.base_command import BaseCommand
 from src.plugin_system.base.component_types import ComponentInfo
@@ -745,6 +746,8 @@ class TarotsPlugin(BasePlugin):
     plugin_name = "tarots_plugin"
     enable_plugin = True
     config_file_name = "config.toml"
+    dependencies = []
+    python_dependencies = []
 
     # 配置节描述
     config_section_descriptions = {
@@ -760,7 +763,7 @@ class TarotsPlugin(BasePlugin):
     # 配置Schema定义
     config_schema = {
         "plugin": {
-            "config_version": ConfigField(type=str, default="1.1.3", description="插件配置文件版本号"),
+            "config_version": ConfigField(type=str, default="1.2.0", description="插件配置文件版本号"),
             "enabled": ConfigField(type=bool, default=True, description="是否启用插件"),
         },
         "components": {
